f429df25212ed0d652c4a4dea83c0b8481938a75
Ricardo Iriarte <lzzow29@gmail.com>
2024-11-27 13:49:11 -0400

    (feat): added loading button status to prevent multiple fetchs

--- src/features/puestoapuesto/components/Form.jsx
+++ src/features/puestoapuesto/components/Form.jsx
@@ -6,6 +6,8 @@ function Form() {
     responsablePhone: '',
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const [formData, setFormData] = useState({
     sede: '',
     funcionarioID: '',
@@ -188,7 +190,7 @@ function Form() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setIsLoading(true);
     const dataToSubmit = new FormData();
 
     // Agregar todos los campos del formulario excepto el archivo
@@ -286,6 +288,8 @@ function Form() {
     } catch (error) {
       console.error('Error en la solicitud:', error);
       alert(error.message || 'Error al enviar los datos');
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -967,8 +971,33 @@ function Form() {
         <button
           type="submit"
           className="w-full bg-blue-500 text-white py-2 px-4 my-4 rounded-md hover:bg-blue-600 transition"
+          disabled={isLoading}
         >
-          Enviar
+          {' '}
+          {isLoading ? (
+            <span className="flex items-center justify-center">
+              {' '}
+              <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                {' '}
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />{' '}
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />{' '}
+              </svg>{' '}
+              Enviando...{' '}
+            </span>
+          ) : (
+            'Enviar'
+          )}{' '}
         </button>
       </form>
     </div>
--- src/features/puestoapuesto/components/ResultsList.jsx
+++ src/features/puestoapuesto/components/ResultsList.jsx
@@ -4,6 +4,7 @@ function ResultsList() {
   const [funcionarios, setFuncionarios] = useState([]);
   const [sedeId, setSedeId] = useState();
   const [nombre, setNombre] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     handleSedes();
@@ -11,6 +12,7 @@ function ResultsList() {
   }, []);
 
   const handleDownload = async () => {
+    setIsLoading(true);
     try {
       const response = await fetch(
         'https://backend-encuesta-flask.onrender.com/api/export_employees',
@@ -38,6 +40,8 @@ function ResultsList() {
       window.URL.revokeObjectURL(url); // Limpiar la URL
     } catch (error) {
       console.error('Error:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -82,8 +86,33 @@ function ResultsList() {
       <button
         onClick={handleDownload}
         className="w-full bg-blue-500 text-white py-2 px-4 rounded-md"
+        disabled={isLoading}
       >
-        Exportar Excel con Resultados
+        {' '}
+        {isLoading ? (
+          <span className="flex items-center justify-center">
+            {' '}
+            <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+              {' '}
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              />{' '}
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              />{' '}
+            </svg>{' '}
+            Cargando...{' '}
+          </span>
+        ) : (
+          'Exportar Excel con Resultados'
+        )}{' '}
       </button>
       <div className="flex flex-row min-w-full mx-auto my-2 space-x-2">
         <select
